346785abd3d91e90bf73175c866e27435501f0f8
Piero <piero.blunda@[D.external@ynap.com>
2019-11-15 16:07:53 +0100

    Rework on app n server files

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+require('dotenv').config();
+process.env.BASE_PATH = __dirname;
+process.env.SERVER_PATH = process.env.BASE_PATH + '/server';
+process.env.VIEWS_PATH = process.env.BASE_PATH + '/client/views';
 var server = require('./server.js');
 server.init();
--- server.js
+++ server.js
@@ -2,12 +2,8 @@
 /* @ToDo: https://blog.cloud66.com/installing-nginx-and-http-2-in-your-local-development-environment/ */
 'use strict';
 const app = require('express')(),
-      environment = require('./server/environment'),
       dbclient = require('./server/dbclient'),
       routes = require('./server/routes'),
-      dotenv = require('dotenv'),
-      Meanivan = require('./framework/meanivan.js'),
-      appRootDir = require('app-root-dir').get(),
       colors = require('colors');
 
 module.exports.init = function(){
@@ -15,42 +11,33 @@ module.exports.init = function(){
   console.log('Starting HTTP server');
   console.log('=============================================================');
 
-  // Init environment variables configuration
-  Meanivan.setEnvironment().then(function(returl){
-    dotenv.config();
-    
-    // Init DB connection
-    process.env.BASEPATH = appRootDir;
-    process.env.SERVERPATH = appRootDir + '/server';
-    process.env.VIEWSPATH = appRootDir + '/client/views';
+  // Init DB connection
+  let dbConfig = {
+    dbName: process.env.DB_NAME
+  };
+  dbclient.connect(dbConfig, function(err){
 
-    let dbConfig = {
-      dbName: process.env.DBNAME
-    };
-    dbclient.connect(dbConfig, function(err){
+    if(err){
+      console.log(colors.red('✗') + ' Error on mongodb connection: ', err);
+      return false;
+    }
 
-      if(err){
-        console.log(colors.red('✗') + ' Error on mongodb connection: ', err);
-        return false;
-      }
+    console.log(colors.green('✓') + ' Connected to mongodb succesfully');
 
-      console.log(colors.green('✓') + ' Connected to mongodb succesfully');
+    // App Config
+    app.set('view engine', 'pug');
+    app.set('views', process.env.VIEWS_PATH);
+    app.set('json spaces', 2);
 
-      // App Config
-      app.set('view engine', 'pug');
-      app.set('views', environment.viewsPath);
-      app.set('json spaces', environment.jsonSpaces);
+    //Route
+    routes.init(app);
 
-      //Route
-      routes.init(app);
-
-      app.listen(process.env.PORT, function(){
-        console.log(colors.green('✓') + ' Environment:     ' + process.env.NODE_ENV);
-        console.log(colors.green('✓') + ' Database:        ' + process.env.DBNAME);
-        console.log(colors.green('✓') + ' Port:            ' + process.env.PORT);
-        console.log(colors.green('\nVisit:         http://localhost:' + process.env.PORT + '\n'));
-      });
-
+    app.listen(process.env.PORT, function(){
+      console.log(colors.green('✓') + ' Environment:     ' + process.env.NODE_ENV);
+      console.log(colors.green('✓') + ' Database:        ' + process.env.DB_NAME);
+      console.log(colors.green('✓') + ' Port:            ' + process.env.PORT);
+      console.log(colors.green('\nVisit:         http://localhost:' + process.env.PORT + '\n'));
     });
+
   });
 };
