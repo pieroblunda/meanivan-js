5a6dab98f61a7363c4a53e0f65a35572f6a79451
Piero Blunda <pieroblunda@gmail.com>
2020-10-11 12:31:56 +0200

    Fix uglify js files

--- scripts/uglify-js.js
+++ scripts/uglify-js.js
@@ -6,25 +6,32 @@ var i18nJsonEnUs = require('../client/i18n/en-US.json');
 var i18nJsonPtBr = require('../client/i18n/pt-BR.json');
 
 // Uglify vendor
-var patterns = [
+var patternsVendor = [
   'node_modules/angular/angular.js',
   'node_modules/angular-sanitize/angular-sanitize.js',
   'node_modules/angular-messages/angular-messages.js',
   'node_modules/angular-translate/dist/angular-translate.js',
 ];
 var globOptions = {}; // any regular node-glob option
-var files = GlobArray.sync(patterns, globOptions);
-var uglified = UglifyJS.minify(files).code;
+var filesVendor = GlobArray.sync(patternsVendor, globOptions);
+var allCodeStringVendor = '';
+
+filesVendor.forEach((file, i) => {
+  allCodeStringVendor += fs.readFileSync(file, "utf8");
+});
+
+var uglified = UglifyJS.minify(allCodeStringVendor).code;
 
 fs.writeFile('public/js/vendor.min.js', uglified, function (err) {
   if (err) return console.log(err);
-  console.log('Done!  public/css/vendor.min.js');
+  console.log('Done!  public/js/vendor.min.js');
 });
+
 
 // -------------------------------
 // Uglify app js
 // -------------------------------
-var patternsB = [
+var patternsApp = [
   'client/app.js',
   'client/services/*.js',
   'client/directives/*.js',
@@ -36,15 +43,18 @@ var patternsB = [
 var ngDictionary = `angular.module('App').constant('DICTIONARY', ${JSON.stringify(i18nJsonEsEs)});`;
 fs.writeFile('client/constants/dictionary.js', ngDictionary, function (err) {
   if (err) return console.log(err);
-  console.log('Done! Dictionarly writed');
+  console.log('Done!  Dictionarly writed');
 });
 
-var globOptionsB = {}; // any regular node-glob option
-var filesB = GlobArray.sync(patterns, globOptions);
-var uglifiedB = UglifyJS.minify(files).code;
+var filesApp = GlobArray.sync(patternsApp, globOptions);
+var allCodeStringApp = '';
+filesApp.forEach((file, i) => {
+  allCodeStringApp += fs.readFileSync(file, "utf8");
+});
+var uglifiedApp = UglifyJS.minify(allCodeStringApp).code;
 
-// Espanol
-fs.writeFile('public/js/app-EN.min.js', uglifiedB, function (err) {
+// English
+fs.writeFile('public/js/app-EN.min.js', uglifiedApp, function (err) {
   if (err) return console.log(err);
   console.log('Done!  public/js/app-EN.min.js');
 });
